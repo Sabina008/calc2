8800c5e9f0c4a0f294890152294b00dead74f5de
Sabina008 <sabi.thiru83@gmail.com>
2021-11-17 21:09:28 -0500

    Updated calc2 for Improved calculator. Added division file, etc.

---
+++ calculator/calculator.py
@@ -0,0 +1,69 @@
+""" This is the increment function"""
+#first import the addition namespace
+from calc.addition import Addition
+from calc.subtraction import Subtraction
+from calc.multiplication import Multiplication
+class Calculator:
+    """ This is the Calculator class"""
+    #this is the calculator static property
+    history = []
+    @staticmethod
+    def get_result_of_first_calculation_added_to_history():
+        """
+        high level support for doing this and that.
+        """
+        return Calculator.history[0].getResult()
+    @staticmethod
+    def clear_history():
+        """
+        high level support for doing this and that.
+        """
+        Calculator.history.clear()
+        return True
+    @staticmethod
+    def history_count():
+        """
+        high level support for doing this and that.
+        """
+        return len(Calculator.history)
+    @staticmethod
+    def add_calculation_to_history(calculation):
+        """
+        high level support for doing this and that.
+        """
+        Calculator.history.append(calculation)
+        return True
+    @staticmethod
+    def get_result_of_last_calculation_added_to_history():
+        """
+        high level support for doing this and that.
+        """
+        # -1 gets the last item added to the list automaticly and you can expect
+        # it to have the get result method
+        return Calculator.history[-1].getResult()
+    @staticmethod
+    def add_number(value_a, value_b):
+        """ adds number to result"""
+        #create an addition object using the factory we created on the calculation class
+        addition = Addition.create(value_a,value_b)
+        # addition = Addition(value_a,value_b) <-this is not good but will work.
+        # It will be repeated too much
+        Calculator.add_calculation_to_history(addition)
+        return Calculator.get_result_of_last_calculation_added_to_history()
+    @staticmethod
+    #this is an example of a calling method
+    def subtract_number(value_a, value_b):
+        """ subtract number from result"""
+        # create an subtraction object using the factory we created on the calculation class
+        subtraction = Subtraction.create(value_a, value_b)
+        # addition = Addition(value_a,value_b) <-this is not good but will work.
+        # It will be repeated too much
+        Calculator.add_calculation_to_history(subtraction)
+        return Calculator.get_result_of_last_calculation_added_to_history()
+    @staticmethod
+    def multiply_numbers(value_a, value_b):
+        """ multiply two numbers and store the result"""
+        #this is a shorthand way to create the multiplication object and added
+        # it the history in one line
+        Calculator.add_calculation_to_history(Multiplication.create(value_a,value_b))
+        return Calculator.get_result_of_last_calculation_added_to_history()
